c9d38eebaa28d27e08b9724e9837a82c6db23c5a
Jiya <jiyajha2020@gmail.com>
2025-05-18 13:56:43 +0530

    take input from user

---
+++ Basics/user_input.cpp
@@ -0,0 +1,14 @@
+//Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+//Examples: Input(user gives value): 7 Output: 7               Input(user gives value): -5    Output: -5
+
+#include <iostream>
+using namespace std;
+void printNumber() {
+    int x;
+    cin >> x;
+    cout << x << endl;
+}
+int main() {
+    printNumber(); 
+    return 0;
+}

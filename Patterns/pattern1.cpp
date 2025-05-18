93fa63144c0cb034bb4b7ba3dd36e0e4fa33e57e
Jiya <jiyajha2020@gmail.com>
2025-05-18 14:52:21 +0530

    pattern1

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -1,4 +1,4 @@
-//printing 9n x n)  '*'
+//Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size nxn filled with '*'. For every value of 'n', help him to print the corresponding n-dimensional forest.
 
 
 #include <bits/stdc++.h>
@@ -7,15 +7,18 @@ using namespace std;
 void printPattern(int n) {
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= n; j++) {
-            cout << "*";
+            cout << "* ";
         }
         cout << endl;
     }
 }
 
 int main() {
-    int n;
-    cin >> n;
-    printPattern(n);
+    int t;
+    cin >> t;
+    for(int i = 0;i<t;i++) {
+        int n;
+        cin >> n;
+        printPattern(n);
     return 0;
 }

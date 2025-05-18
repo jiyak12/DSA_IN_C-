f8d62f10af1f7ee503248b9f3bb0362ed0b1e183
Jiya <jiyajha2020@gmail.com>
2025-05-18 16:13:39 +0530

    Create pattern8

---
+++ Patterns/pattern8.cpp
@@ -0,0 +1,36 @@
+/*
+*************
+ *********** 
+  *********  
+   *******   
+    *****    
+     ***     
+      *      
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <i; j++) {
+            cout <<" ";
+        }
+        for (int j = 0; j <2*n-(2*i+1); j++) {
+            cout <<"*";
+        }
+        for (int j = 0; j <i; j++) {
+            cout <<" ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for (int i = 0; i <= t; i++) {
+      int n;
+      cin >> n;
+      printPattern(n);
+    }  
+}

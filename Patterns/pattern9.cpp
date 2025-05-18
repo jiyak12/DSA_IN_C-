eb7ff686820df6481e3ea1ae0e1e42444e3dede1
Jiya <jiyajha2020@gmail.com>
2025-05-18 16:40:27 +0530

    Create pattern9.cpp

---
+++ Patterns/pattern9.cpp
@@ -0,0 +1,51 @@
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+  */
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    // Top Half (Normal Pyramid)
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Bottom Half (Inverted Pyramid) - start from i = n (repeat middle row)
+    for (int i = n; i >= 0; i--) {
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        printPattern(n);
+    }
+    return 0;
+}

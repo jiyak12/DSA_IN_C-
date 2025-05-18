3c33f981464d8c15ce70c41cbf6b99f7da19d12b
Jiya <jiyajha2020@gmail.com>
2025-05-18 14:03:29 +0530

    Create if_else_statements.cpp

---
+++ Basics/if_else_statements.cpp
@@ -0,0 +1,43 @@
+/*Given marks of a student, print on the screen:
+Grade A if marks >= 90 Grade B if marks >= 70 Grade C if marks >= 50 Grade D if marks >= 35 Fail, otherwise. 
+
+Examples:
+Input: marks = 95    Output: Grade A
+Explanation: marks are greater than or equal to 90.
+
+Input: marks = 14    Output: Fail
+Explanation: marks are less than 35. */  
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void studentGrade(int marks) {
+        if (marks >= 90) {
+            cout << "Grade A";
+        }
+        else if (marks >= 70) {
+            cout << "Grade B";
+        }
+        else if (marks >= 50) {
+            cout << "Grade C";
+        }
+        else if (marks >= 35) {
+            cout << "Grade D";
+        }
+        else {
+            cout << "Fail";
+        }
+    }
+};
+
+int main() {
+    int marks;
+    cin >> marks;
+
+    Solution obj;
+    obj.studentGrade(marks); // call the method using an object of Solution
+
+    return 0;
+}

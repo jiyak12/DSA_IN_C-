5428c9d07fff561aacb89b9e92c8f9bc5d7821bc
Jiya <jiyajha2020@gmail.com>
2025-05-28 22:53:35 +0530

    left rotate

---
+++ Array/prog5.cpp
@@ -0,0 +1,34 @@
+// left rotate an array
+/* Here, in the given array :
+n = 5,
+arr[] = {1,2,3,4,5}
+At first, shift the array towards the left and store the value of the first index in a variable (let it be x).
+Then, iterate the array from the 0th index to the n-1th index.
+And then store the value present in the next index to the current index like this : arr[i] = arr[i+1]
+And to prevent its segmentation fault,iterate it till n-1.
+At last, put the value of variable x in the last index of the array.
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+void solve(int arr[], int n) {
+  int temp = arr[0]; // storing the first element of array in a variable
+  for (int i = 0; i < n - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[n - 1] = temp; // assigned the value of variable at the last index
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+
+}
+int main() {
+  int n=5;
+  
+  int arr[]= {1,2,3,4,5};
+  solve(arr, n);
+}
+
+

10827799e0c61c4100560c60cb8f62e9b9b43e2d
Jiya <jiyajha2020@gmail.com>
2025-05-30 02:25:16 +0530

    finding no that appears once and others twice

---
+++ Array/prog13.cpp
@@ -0,0 +1,29 @@
+ //Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
+//optimal solution uses xor - if we perform the XOR of all the numbers of the array elements, we will be left with a single number.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int> &arr) {
+    //size of the array:
+    int n = arr.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
+
+

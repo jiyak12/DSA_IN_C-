50d14ae8cb0468bcf31f569758535e68b51bf30d
Jiya <jiyajha2020@gmail.com>
2025-05-30 03:02:56 +0530

    2sum

---
+++ Array/prog14.cpp
@@ -0,0 +1,27 @@
+//two sum
+// optimal strategy would be by using two pointers if asked yes/no otherwise using map is a better approach.
+#include <bits/stdc++.h>
+using namespace std;
+
+string twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return "YES"; //return {mpp[moreNeeded], i}; when asked about element with index format
+        }
+        mpp[num] = i;
+    }
+    return "NO"; //return { -1, -1};
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 1: " << ans << endl;
+    return 0;
+}

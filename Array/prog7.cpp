20dfb018d4a2383d5c6a7be8361138cfa3ebc7e1
Jiya <jiyajha2020@gmail.com>
2025-05-29 23:53:31 +0530

    move all zeroes to the end of array

---
+++ Array/prog7.cpp
@@ -0,0 +1,51 @@
+//moving zeroes to the end of array 
+// brute force- iterate through the array and store all the non zero numbers then put all the non zero numbers to the front and put all the zeros at the remaining places in the array
+ /* optimal approach -First, using a loop, we will place the pointer j. If we don’t find any 0, we will not perform the following steps.
+After that, we will point i to index j+1 and start moving the pointer using a loop.
+While moving the pointer i, we will do the following:
+If a[i] != 0 i.e. a[i] is a non-zero element: We will swap a[i] and a[j]. Now, the current j is pointing to the non-zero element a[i]. So, we will shift the pointer j by 1 so that it can again point to the first zero.
+Finally, our array will be set in the right manner.*/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> moveZeros(int n, vector<int> a) {
+    int j = -1;
+    //place the pointer j:
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    //no non-zero elements:
+    if (j == -1) return a;
+
+    //Move the pointers i and j
+    //and swap accordingly:
+    for (int i = j + 1; i < n; i++) {
+        if (a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
+}
+
+
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n = 10;
+    vector<int> ans = moveZeros(n, arr);
+    for (auto &it : ans) {
+        cout << it << " ";
+    }
+    cout << '\n';
+    return 0;
+}
+

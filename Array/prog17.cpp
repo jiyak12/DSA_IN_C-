615fb77cefdd220e42a06444eefee28d13232f68
Jiya <jiyajha2020@gmail.com>
2025-06-01 00:40:36 +0530

    maximum subarray sum

---
+++ Array/prog17.cpp
@@ -0,0 +1,46 @@
+//max subarray sum 
+/* optimal solution- kadane's algorithm
+We will run a loop(say i) to iterate the given array.
+Now, while iterating we will add the elements to the sum variable and consider the maximum one.
+If at any point the sum becomes negative we will set the sum to 0 as we are not going to consider it as a part of our answer.
+  A subarray with a sum less than 0 will always reduce our answer and so this type of subarray cannot be a part of the subarray with maximum sum.*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long maxSubarraySum(int arr[], int n) {
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    long long maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+

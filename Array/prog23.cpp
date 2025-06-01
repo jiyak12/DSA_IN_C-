14e73ef202c91c72c6ec9c9ddeb1bd104a389205
Jiya <jiyajha2020@gmail.com>
2025-06-02 01:49:31 +0530

    spiral traversal of matrix

---
+++ Array/prog23.cpp
@@ -0,0 +1,97 @@
+//spiral of a matrix
+/*
+In this approach, we will be using four loops to print all four sides of the matrix.
+top = starting row index (0)
+
+bottom = ending row index (n-1)
+
+left = starting column index (0)
+
+right = ending column index (m-1)
+
+
+Top Row:
+Print elements from left to right in the top row.
+→ Then increment top to move to the next inner row.
+
+Right Column:
+Print elements from top to bottom in the right column.
+→ Then decrement right to move to the inner column.
+
+Bottom Row (if top <= bottom):
+Print elements from right to left in the bottom row.
+→ Then decrement bottom.
+
+Left Column (if left <= right):
+Print elements from bottom to top in the left column.
+→ Then increment left.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> printSpiral(vector<vector<int>> mat) {
+ 
+  // Define ans array to store the result.
+  vector<int> ans;
+ 
+  int n = mat.size(); // no. of nows
+  int m = mat[0].size(); // no. of columns
+  
+  // Initialize the pointers reqd for traversal.
+  int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+  // Loop until all elements are not traversed.
+  while (top <= bottom && left <= right) {
+      
+    // For moving left to right
+    for (int i = left; i <= right; i++)
+      ans.push_back(mat[top][i]);
+
+    top++;
+
+    // For moving top to bottom.
+    for (int i = top; i <= bottom; i++)
+      ans.push_back(mat[i][right]);
+
+    right--;
+    
+    // For moving right to left.
+    if (top <= bottom) {
+      for (int i = right; i >= left; i--)
+       ans.push_back(mat[bottom][i]);
+
+      bottom--;
+    }
+
+    // For moving bottom to top.
+    if (left <= right) {
+      for (int i = bottom; i >= top; i--)
+        ans.push_back(mat[i][left]);
+
+      left++;
+    }
+  }
+  return ans;
+}
+
+int main() {
+    
+  //Matrix initialization.
+  vector<vector<int>> mat   {{1, 2, 3, 4},
+                             {5, 6, 7, 8},
+	                         {9, 10, 11, 12},
+		                     {13, 14, 15, 16}};
+		                     
+  vector<int> ans = printSpiral(mat);
+
+  for(int i = 0;i<ans.size();i++){
+      
+      cout<<ans[i]<<" ";
+  }
+  
+  cout<<endl;
+  
+  return 0;
+}

eb3cb2e3b4d10a59cfea9beeb1efb47e36b89652
Jiya <jiyajha2020@gmail.com>
2025-06-05 01:46:45 +0530

    selection sort

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,40 @@
+//Selection Sort is a simple comparison-based sorting algorithm. It works by repeatedly selecting the minimum (or maximum) element from the unsorted portion of the array and moving it to the correct position in the sorted portion.
+//For each position i in the array from 0 to n-2:
+//Find the minimum element from i to n-1.
+//Swap it with the element at position i.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+void selection_sort(int arr[], int n) {
+  // selection sort
+  for (int i = 0; i < n - 1; i++) {
+    int mini = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[mini]) {
+        mini = j;
+      }
+    }
+    int temp = arr[mini];
+    arr[mini] = arr[i];
+    arr[i] = temp;
+  }
+
+  cout << "After selection sort: " << "\n";
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+
+}
+int main() {
+  int arr[] = {13,46,24,52,20,9};
+  int n = sizeof(arr) / sizeof(arr[0]);
+   cout << "Before selection sort: " << "\n";
+   for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+  selection_sort(arr, n);
+  return 0;
+}

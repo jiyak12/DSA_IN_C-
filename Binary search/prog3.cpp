83320c8ab7b9102aef7031d57fcd8e933e1d4b17
Jiya <jiyajha2020@gmail.com>
2025-06-06 00:14:35 +0530

    implement upper bound

---
+++ Binary search/prog3.cpp
@@ -0,0 +1,34 @@
+//The upper bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than the given key i.e. x.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
+
+

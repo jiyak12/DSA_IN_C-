2f359e781905d80e0705e41f37c7ca651793585c
Jiya <jiyajha2020@gmail.com>
2025-06-08 14:50:52 +0530

    painter's partition problem

---
+++ Binary search/prog23.cpp
@@ -0,0 +1,57 @@
+//painter's partition problem
+
+// Given an array/list of length ‘N’, where the array/list represents the boards and each element of the given array/list represents the length of each board. Some ‘K’ numbers of painters are available to paint these boards. Consider that each unit of a board takes 1 unit of time to paint. You are supposed to return the area of the minimum time to get this job done of painting all the ‘N’ boards under the constraint that any painter will only paint the continuous sections of boards.
+/*Eliminate the halves based on the number of painters returned by countPainters():
+pass the potential value of time, represented by the variable 'mid', to the ‘countPainters()' function. 
+This function will return the number of painters to paint all the boards.
+If painters > k: On satisfying this condition,the number ‘mid’ is smaller than our answer.
+So, eliminate the left half and consider the right half(i.e. low = mid+1).
+for min value, eliminate the right half and consider the left half(i.e. high = mid-1).*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPainters(vector<int> &boards, int time) {
+    int n = boards.size(); //size of array.
+    int painters = 1;
+    long long boardsPainter = 0;
+    for (int i = 0; i < n; i++) {
+        if (boardsPainter + boards[i] <= time) {
+            //allocate board to current painter
+            boardsPainter += boards[i];
+        }
+        else {
+            //allocate board to next painter
+            painters++;
+            boardsPainter = boards[i];
+        }
+    }
+    return painters;
+}
+
+int findLargestMinDistance(vector<int> &boards, int k) {
+    int low = *max_element(boards.begin(), boards.end());
+    int high = accumulate(boards.begin(), boards.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int painters = countPainters(boards, mid);
+        if (painters > k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> boards = {10, 20, 30, 40};
+    int k = 2;
+    int ans = findLargestMinDistance(boards, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
+
